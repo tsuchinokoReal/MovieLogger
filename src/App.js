9fb8559078b73a96517d6ec0eeb1755c255d911c
Parker <35046437+tsuchinokoReal@users.noreply.github.com>
2023-12-11 21:32:15 -0500

    initial commit (ugly version)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [formData, setFormData] = useState({ title: '', director: '' });
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/movies');
+      setMovies(response.data.movies);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleAddMovie = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:3001/api/log-movie', formData);
+      fetchMovies();
+      setFormData({ title: '', director: '' });
+    } catch (error) {
+      console.error('Error adding movie:', error);
+    }
+  };
+
+  const handleUpdateMovie = async (id) => {
+    try {
+      await axios.put(`http://localhost:3001/api/movies/${id}`, formData);
+      fetchMovies(); 
+      setFormData({ title: '', director: '' });
+    } catch (error) {
+      console.error('Error updating movie:', error);
+    }
+  };
+
+  const handleDeleteMovie = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/api/movies/${id}`);
+      fetchMovies(); 
+    } catch (error) {
+      console.error('Error deleting movie:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movie Logger App</h1>
+
+      <form onSubmit={handleAddMovie}>
+        <label>
+          Title:
+          <input type="text" name="title" value={formData.title} onChange={handleInputChange} required />
+        </label>
+        <label>
+          Director:
+          <input type="text" name="director" value={formData.director} onChange={handleInputChange} required />
+        </label>
+        <button type="submit">Add Movie</button>
+      </form>
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie._id}>
+            <div>
+              <p>Title: {movie.title}</p>
+              <p>Director: {movie.director}</p>
+            </div>
+            <div>
+              <button onClick={() => handleUpdateMovie(movie._id)}>Update</button>
+              <button onClick={() => handleDeleteMovie(movie._id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://prettir:kaiwhite@cluster0.wuhslnx.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://prettir:kaiwhite@cluster0.wuhslnx.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+// Create
+app.post('/api/log-movie', async (req, res) => {
+  try {
+    const { title, director } = req.body;
+
+    if (!title || !director) {
+      return res.status(400).json({ error: 'Title and director are required.' });
+    }
+
+    const newMovie = new Movie({ title, director });
+    await newMovie.save();
+
+    res.status(201).json(newMovie);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Read
+app.get('/api/movies', async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.json({ movies });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Update
+app.put('/api/movies/:id', async (req, res) => {
+  try {
+    const { title, director } = req.body;
+    const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, { title, director }, { new: true });
+    res.json(updatedMovie);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Delete
+app.delete('/api/movies/:id', async (req, res) => {
+  try {
+    await Movie.findByIdAndDelete(req.params.id);
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
